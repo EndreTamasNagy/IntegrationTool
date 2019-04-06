a25c929e69435fbc15a7d5c7f246489b680af609
Endre <tamas.nagy.endre@gmail.com>
2019-04-06 16:36:44 +0200

    from Home

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ fullscreen: true})
+  win = new BrowserWindow({})
 
+  win.maximize();
   // and load the index.html of the app.
   win.loadFile('index.html')
 
